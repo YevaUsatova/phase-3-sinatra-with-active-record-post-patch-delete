822a6435a781ffd173b985c78975e2ce84e3a98d
YevaUsatova <yeva.usatova@gmail.com>
2021-10-31 13:34:51 -0400

    commit tree e78e17cfa8699e77b2e9b7224cfbf74af3398d56

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,26 @@ class ApplicationController < Sinatra::Base
         user: { only: [:name] }
       } }
     })
-  end
 
+  end
+  delete '/reviews/:id' do
+    Review.find(params[:id]).destroy.to_json
+  end
+  post '/reviews' do
+    review = Review.create(
+    score: params[:score],
+    comment: params[:comment],
+    game_id: params[:game_id],
+    user_id: params[:user_id]
+  )
+  review.to_json
+  end
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+  review.update(
+    score: params[:score],
+    comment: params[:comment]
+  )
+  review.to_json
+  end
 end
